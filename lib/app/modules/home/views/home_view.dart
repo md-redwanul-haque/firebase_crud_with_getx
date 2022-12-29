b7729052993144d33d5addd8f28e019ab381940a
md-redwanul-haque <redwan229900@gmail.com>
2022-12-29 17:56:25 +0600

    Initial commit

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(fontSize: 17),
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                initialValue: "Pass",
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(fontSize: 17),
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(onPressed: (){}, child: Text(" Login ")),
+                ElevatedButton(onPressed: (){}, child: Text(" Registration "))
+              ],
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
